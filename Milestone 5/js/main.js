aa2151d1fecd5b94dad4ab75dad7f47ef45db9d4
salvatorebriga <salvo.z2302@gmail.com>
2024-05-26 01:01:05 +0200

    Milestone 5

--- Milestone 5/js/main.js
+++ Milestone 5/js/main.js
@@ -13,16 +13,19 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Hai portato a spasso il cane?",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Ricordati di stendere i panni",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 16:15:22",
               message: "Tutto fatto!",
               status: "received",
+              isMenuVisible: false,
             },
           ],
         },
@@ -35,16 +38,19 @@ createApp({
               date: "20/03/2020 16:30:00",
               message: "Ciao come stai?",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "20/03/2020 16:30:55",
               message: "Bene grazie! Stasera ci vediamo?",
               status: "received",
+              isMenuVisible: false,
             },
             {
               date: "20/03/2020 16:35:00",
               message: "Mi piacerebbe ma devo andare a fare la spesa.",
               status: "sent",
+              isMenuVisible: false,
             },
           ],
         },
@@ -57,16 +63,19 @@ createApp({
               date: "28/03/2020 10:10:40",
               message: "La Marianna va in campagna",
               status: "received",
+              isMenuVisible: false,
             },
             {
               date: "28/03/2020 10:20:10",
               message: "Sicuro di non aver sbagliato chat?",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "28/03/2020 16:15:22",
               message: "Ah scusa!",
               status: "received",
+              isMenuVisible: false,
             },
           ],
         },
@@ -79,11 +88,13 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Lo sai che ha aperto una nuova pizzeria?",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Si, ma preferirei andare al cinema",
               status: "received",
+              isMenuVisible: false,
             },
           ],
         },
@@ -96,11 +107,13 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Ricordati di chiamare la nonna",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Va bene, stasera la sento",
               status: "received",
+              isMenuVisible: false,
             },
           ],
         },
@@ -113,16 +126,19 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Ciao Claudia, hai novità?",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Non ancora",
               status: "received",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 15:51:00",
               message: "Nessuna nuova, buona nuova",
               status: "sent",
+              isMenuVisible: false,
             },
           ],
         },
@@ -135,11 +151,13 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Fai gli auguri a Martina che è il suo compleanno!",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Grazie per avermelo ricordato, le scrivo subito!",
               status: "received",
+              isMenuVisible: false,
             },
           ],
         },
@@ -152,16 +170,19 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Ciao, andiamo a mangiare la pizza stasera?",
               status: "received",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
               status: "sent",
+              isMenuVisible: false,
             },
             {
               date: "10/01/2020 15:51:00",
               message: "OK!!",
               status: "received",
+              isMenuVisible: false,
             },
           ],
         },
@@ -172,10 +193,6 @@ createApp({
       searchQuery: "",
     };
   },
-
-  /*mounted() {
-    getLastMessage();
-  },*/
 
   computed: {
     filteredChats() {
@@ -220,6 +237,10 @@ createApp({
       return messages.length
         ? messages[messages.length - 1].date
         : "Nessuna data";
+    },
+
+    toggleMenu(messages) {
+      messages.isMenuVisible = !messages.isMenuVisible;
     },
   },
 }).mount("#app");
